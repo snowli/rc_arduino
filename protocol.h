321cc58d5684e7e77ea8b3b2f95db9d2f41fd1a7
Ram <rhombusj@gmail.com>
2012-12-01 15:17:19 -0500

    Draft EEPROM interface to store array data

---
+++ protocol.h
@@ -0,0 +1,75 @@
+/*
+@desc
+@author Sriram Jayakumar
+@date 11/30/2012
+@ver 1.0
+@bugs
+*/
+
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#include "Arduino.h"
+
+class Protocol
+{
+	private:
+		//**Status variables**
+		boolean _initialized = false;
+		boolean _written = false;
+		
+		//**Array**
+		char _dim;
+		
+		//**EEPROM**
+		int _startAddr;
+		
+		//EEPROM address to be written and value to be written
+		int _currAddr;
+		int _currVal = 0;
+		
+		//Which bit in the byte needs to be written next
+		int _slot = 0;
+		
+	public:
+		/*
+		@desc. Provides read and write access to an nxn array of booleans stored in EEPROM. See init(char dim, int startAddr) for how Protocol instances will differ. The first byte stores the dimesnion n of the array. Subsequent bits store booleans, row by row.
+		@future improvements
+			-Allow 
+				rectangular arrays.
+				arbitrary data types to be stored in the array.
+				out of order writes to any array index.
+			-Check for hardware failures.
+		*/
+		Protocol();
+		
+		/*
+		@desc. Initializes with parameters.
+		@in
+			-dim Length of one side of the nxn array. 0 <= dim <= 90 must be true. 90 is the upper limit because there are only 1023 bytes available for the array data with this formatting scheme.
+			-startAddr The address in EEPROM to start writing data at. This makes it somewhat easier to avoid writing the same addresses of EEPROM each time. 0 <= startAddr <= 1024 must hold true.
+		@out Whether the initialization was successful. This depends on valid input.
+		*/
+		boolean init(char dim, int startAddr);
+		
+		/*
+		@desc Flushes any outstanding data to EEPROM, since there are no guarantees on when data is written. An init(...) which returns true must be called before flush(). This must be called when all writes are finished to guarantee everything is pushed to EEPROM.
+		@out Whether the flush succeeded.
+		*/
+		boolean flush();
+		
+		/*
+		@desc Dumps data to the serial port in a comma-separated list of bytes. An init(...) which returns true must be called before dump().
+		@out Whether the dump succeeded.
+		*/
+		boolean dump();
+		
+		/*
+		@desc Queues an array value to be written. An init(...) which returns true must be called before write(...).
+		@out Whether the write was successful.
+		*/
+		boolean write(boolean val);
+}
+
+
+#endif
