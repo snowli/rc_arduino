8efafd42a384d1bd0a0a522a0d735a9e373e9478
snow <thesnowmobile4@gmail.com>
2012-12-02 12:36:03 -0500

    fixed explore algo

--- explore/explore.cpp
+++ explore/explore.cpp
@@ -3,12 +3,17 @@
 extern char *grid = NULL;
 extern uint8_t curr_x = X_START;
 extern uint8_t curr_y = Y_START;
+extern vector_t dirs_vec[4] = {{0,1},{-1,0},{0,-1},{1,0}};
 
 void initialize_grid()
 {
     // *4 because we could be starting anywhere in the grid, and this covers it so that we can go at least EXPLORE_RADIUS in any direction
     grid = (char *)malloc( sizeof(node_t) * EXPLORE_RADIUS * EXPLORE_RADIUS * 4);
     memset(grid, 0, sizeof(node_t) * EXPLORE_RADIUS*EXPLORE_RADIUS*4);
+
+    // set the start position to status explored
+    node_t *node = (node_t *)(grid +(X_START + Y_START*EXPLORE_RADIUS*2)*sizeof(node_t));
+    node->is_explored = 1;
 }
 
 void free_grid()
@@ -19,23 +24,29 @@ void free_grid()
 // @param x - x location to be moved into
 // @param y - y location to be moved into
 // @return - 1 on success, 0 failure
-int move_forward_block(uint8_t x, uint8_t y)
+int move_forward_block(int8_t x, int8_t y)
 {
+    Serial.print("x: ");
+    Serial.print(x);
+    Serial.print("y: ");
+    Serial.println(y);
     curr_x = x;
     curr_y = y;
     int steps = BLOCK_SIZE / STEP_SIZE;
     int i;
     int j;
-    node_t *node = (node_t *)(grid + x + y*EXPLORE_RADIUS*2);
+    node_t *node = (node_t *)(grid + (x + y*EXPLORE_RADIUS*2)*sizeof(node_t));
     
-    if( !node->is_explored )
+    if( !(node->is_explored) )
     {
+        node->is_explored = 1;
         for( i=0; i<steps; i++)
         {
             for( j=0; j<UNITS_PER_STEP; j++)
             {
-                if(node->is_obstructed)
+                if((node->is_obstructed))
                 {
+                    Serial.println("OBSTRUCTION FOUND");
                     for(;i>0;i--)
                     {
                         for(j=0; j<UNITS_PER_STEP; j++)
@@ -48,11 +59,11 @@ int move_forward_block(uint8_t x, uint8_t y)
                 unitForward();
             }
         }
-        node->is_explored = 1;
         return 1;
     }
     else
     {
+        Serial.println("already explored");
         return 0;
     }
     
@@ -73,59 +84,109 @@ void move_backward_block()
     }
 }
 
-void explore(uint8_t x, uint8_t y)
-{   
-    if( x == EXPLORE_RADIUS*2 || x < 0 
-        || y > EXPLORE_RADIUS*2 || y < 0)
-        return;
-
-    //FORWARD
-    Serial.println("Forward");
-    if(move_forward_block(x, y+1))
+// @return 1 in bounds, 0 out of bounds
+int is_in_bounds(int8_t x, int8_t y, int8_t dir)
+{
+    switch( dir )
     {
-        explore(x, y+1);
-        move_backward_block();
+        case 0:
+            return !(y > EXPLORE_RADIUS*2-1); 
+        break;
+        case 1:
+            return !(x < 0);
+        break;
+        case 3:
+            return !(x > EXPLORE_RADIUS*2-1);
+        break;
+        case 2:
+            return !(y < 0);
+        break;
+        default:
+            return 1;
+    }
+
+    return 0;
+}
+
+void explore(int8_t x, int8_t y, int8_t dir)
+{   
+    //FORWARD
+    int8_t x0 = x + dirs_vec[dir].x;
+    int8_t y0 = y + dirs_vec[dir].y;
+    if( is_in_bounds(x0, y0, dir) )
+    {
+        Serial.println(dir);
+        if(move_forward_block(x0, y0))
+        {
+            explore(x0, y0, dir);
+            move_backward_block();
+        }
     }
 
     //LEFT
-    Serial.println("Left");
-    rotate(90);
-    if(move_forward_block(x-1, y))
+    int8_t dir1 = (dir+1)%4;
+    int8_t x1 = x + dirs_vec[dir1].x;
+    int8_t y1 = y + dirs_vec[dir1].y;
+    rotateLeft();
+    if( is_in_bounds(x1, y1, dir1) )
     {
-        explore(x-1, y);
-        rotate(180);
-        move_forward_block(x, y);
-    }
-    else
-    {
-        rotate(180);
+        Serial.println(dir1);
+        if(move_forward_block(x1, y1))
+        {
+            explore(x1, y1, dir1);
+            rotateLeft();
+            rotateLeft();
+            move_forward_block(x1, y1);
+        }
+        else
+        {
+            rotateLeft();
+            rotateLeft();
+        }
     }
 
     //RIGHT
-    Serial.println("Right");
-    if(move_forward_block(x+1, y))
+    int8_t dir2 = (dir+3)%4;
+    int8_t x2 = x + dirs_vec[dir2].x;
+    int8_t y2 = y + dirs_vec[dir2].y;
+    if( is_in_bounds(x2, y2, dir2) )
     {
-        explore(x+1, y);
-        move_backward_block();
+        Serial.println(dir2);
+        if(move_forward_block(x2, y2))
+        {
+            explore(x2, y2, dir2);
+            move_backward_block();
+        }
     }
-    rotate(270);
+    rotateLeft();
+    rotateLeft();
+    rotateLeft();
 
     //BACK
-    Serial.println("Back");
-    if(move_forward_block(x, y-1))
+    int8_t dir3 = (dir+2)%4;
+    int8_t x3 = x + dirs_vec[dir3].x;
+    int8_t y3 = y + dirs_vec[dir3].y;
+    if( is_in_bounds(x3, y3, dir3) )
     {
-        explore(x, y-1);
-        move_backward_block();
-        rotate(180);
-    }
-    else
-    {
-        rotate(90);
+        Serial.println(dir3);
+        if(move_forward_block(x3, y3))
+        {
+            explore(x3, y3, dir3);
+            move_backward_block();
+            rotateLeft();
+            rotateLeft();
+        }
+        else
+        {
+            rotateLeft();
+            rotateLeft(); //TODO verify that this should actually be this 180 turn
+        }
     }
 }
 
 void interrupt_handler_explore()
 {
+    Serial.println("INTERRUPT!!");
     node_t *curr_node = (node_t *)(grid + curr_x + curr_y*EXPLORE_RADIUS*2);
     curr_node->is_obstructed = 1;
 }
--- explore/explore.h
+++ explore/explore.h
@@ -8,11 +8,11 @@
 #define STEP_SIZE 1
 #define UNITS_PER_STEP 1
 
-#define X_START 1
-#define Y_START 0
+#define X_START 2
+#define Y_START 2
 
 #include <Arduino.h>
-#include "movement_api.h"
+#include <movement_api.h>
 
 struct node_t{
     // 0 not explored
@@ -23,15 +23,21 @@ struct node_t{
     uint8_t is_obstructed;
 };
 
+struct vector_t{
+    uint8_t x;
+    uint8_t y;
+};
+
+extern vector_t dirs_vec[4];
 extern char *grid;
 extern uint8_t curr_x;
 extern uint8_t curr_y;
 
 void initialize_grid();
 void free_grid();
-int move_forward_block(uint8_t x, uint8_t y);
+int move_forward_block(int8_t x, int8_t y);
 void move_backward_block();
-void explore(uint8_t x, uint8_t y);
+void explore(int8_t x, int8_t y, int8_t dir);
 void interrupt_handler_explore();
 
 #endif
