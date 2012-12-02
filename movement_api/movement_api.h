97ac482406e0c01fec2ca98dd7612738ac0d712d
Ram <rhombusj@gmail.com>
2012-12-01 20:20:04 -0500

    Manual rotate and initialize in API

--- movement_api/movement_api.h
+++ movement_api/movement_api.h
@@ -1,23 +1,32 @@
 /*
 Prototype Movement API. Provide rotate, unitForward, unitReverse.
 
-@author Sriram Jayakumar
+@author Sriram Jayakumar, Snow Li
 @date 11/10/2012
    */
 
 #ifndef _MOVEMENT_API_H_
 #define _MOVEMENT_API_H_
 
+/*
+ROTATE_LEFT_COMPLETE -- User input to specify when a manual left rotation is complete. The input should be low when the rotation is complete.
+LEFT_LED -- Indicates the car is requesting a left rotate
+*/
 #define FORWARD_PIN 4
 #define REVERSE_PIN 5
 #define LEFT_PIN 6
 #define RIGHT_PIN 7
-#define FRONT_BUMPER 3
+#define LEFT_LED 8
+#define ROTATE_LEFT_COMPLETE 9
 
 #define ROTATE_ALLOWABLE_MARGIN 20
 
+//Define MANUAL_ROTATE to let an external user rotate the car
+//whenever a rotation is required.
+#define MANUAL_ROTATE
+
 #include <Arduino.h>
-#include "/usr/share/arduino/libraries/Wire/Wire.h"
+#include <Wire.h>
 
 //Compass hardware address
 //The Wire library uses 7-bit I2C addressing, rather than 8-bits
@@ -25,6 +34,15 @@ const char compassAddr = 0x42>>1;
 
 void unitForward();
 void unitReverse();
-boolean rotate(int degree);
+
+/*
+@desc Rotates left by 90 degrees
+*/
+void rotateLeft();
+
+/*
+@desc Initialize the car for movement. Sets up pins etc.
+*/
+void initialize();
 
 #endif
